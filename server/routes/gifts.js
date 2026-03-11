0bcecd6c3265d883f0820f2c88e0163eff01026b
Andy Cordero <corderoandy5@gmail.com>
2026-03-11 19:45:34 -0400

    lab 4 complete

--- server/routes/gifts.js
+++ server/routes/gifts.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import GiftsController from '../controllers/gifts.js'
+import GiftsController from '../controllers/gifts.js';
 
 const router = express.Router();
 
-router.get('/', GiftsController.getGifts)
-
-router.get('/:giftId', GiftsController.getGiftById);
+router.get('/', GiftsController.getGifts);
+router.get('/:id', GiftsController.getGiftById);
+router.post('/', GiftsController.createGift);
+router.put('/:id', GiftsController.updateGift);
+router.delete('/:id', GiftsController.deleteGift);
 
 export default router;
--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ const app = express();
 
 app.use(cors());
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.status(200).send('<h1 style="text-align: center; margin-top: 50px;">UnEarthed API</h1>');
 });
