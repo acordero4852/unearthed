815370bb4b20877cf4fc105dd902e83e1df053ca
Andy Cordero <corderoandy5@gmail.com>
2026-06-03 08:57:42 -0400

    lab part 3 complete

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,9 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: '../server/public',
-    emptyOutDir: true
-  },
+  plugins: [react()],
   server: {
     proxy: {
       '/gifts': {
@@ -12,4 +11,4 @@ export default defineConfig({
       }
     }
   }
-});
+})
